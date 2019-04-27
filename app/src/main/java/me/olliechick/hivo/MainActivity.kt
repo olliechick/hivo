1ab930e90f2f20ac956fb5e5b2ddf81309e8c261
Ollie Chick <och26@uclive.ac.nz>
2019-04-27 21:22:35 +1200

    Animate record button and add linearlayout with text views and edit text to specify buffer

--- app/src/main/java/me/olliechick/hivo/MainActivity.kt
+++ app/src/main/java/me/olliechick/hivo/MainActivity.kt
@@ -3,26 +3,45 @@ package me.olliechick.hivo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import android.animation.AnimatorInflater
+import android.animation.ValueAnimator
+import android.graphics.drawable.LayerDrawable
+import android.graphics.drawable.GradientDrawable
+
 
 class MainActivity : AppCompatActivity() {
 
-    var recording = false
+    private var isRecording = false
+    private lateinit var valueAnimator: ValueAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layerDrawable = getDrawable(R.drawable.rec_button) as LayerDrawable
+        val gradientDrawable = layerDrawable.findDrawableByLayerId(R.id.redCircle) as GradientDrawable
+
+        valueAnimator = AnimatorInflater.loadAnimator(
+            this, R.animator.rec_button_transition
+        ) as ValueAnimator
+
+        valueAnimator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Float
+            gradientDrawable.cornerRadius = progress
+            recordButton.setImageDrawable(layerDrawable)
+        }
     }
 
     fun recordClicked(v: View) {
-        if (recording) {
-            recordButton.setImageDrawable(getDrawable(R.drawable.rec_button))
-            recording = false
+        if (isRecording) {
+            valueAnimator.reverse()
+            isRecording = false
         } else {
-            recordButton.setImageDrawable(getDrawable(R.drawable.rec_button_recording))
-            recording = true
+            valueAnimator.start()
+            isRecording = true
         }
     }
+
 
 }
