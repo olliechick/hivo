e53a98cb4deb38c915e8bc5cd1dcc410c1302b5b
Ollie Chick <och26@uclive.ac.nz>
2019-04-27 18:06:09 +1200

    Create the record button, that swaps between red circle and red rounded rectangle.

--- app/src/main/java/me/olliechick/hivo/MainActivity.kt
+++ app/src/main/java/me/olliechick/hivo/MainActivity.kt
@@ -2,11 +2,27 @@ package me.olliechick.hivo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var recording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun recordClicked(v: View) {
+        if (recording) {
+            recordButton.setImageDrawable(getDrawable(R.drawable.rec_button))
+            recording = false
+        } else {
+            recordButton.setImageDrawable(getDrawable(R.drawable.rec_button_recording))
+            recording = true
+        }
+    }
+
 }
