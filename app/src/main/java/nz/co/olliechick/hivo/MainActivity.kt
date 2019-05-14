e59e937be35a11b6240129027b40d331356cfcfb
Ollie Chick <och26@uclive.ac.nz>
2019-05-15 02:05:06 +1200

    Add main activity

--- app/src/main/java/nz/co/olliechick/hivo/MainActivity.kt
+++ app/src/main/java/nz/co/olliechick/hivo/MainActivity.kt
@@ -1,69 +1,102 @@
 package nz.co.olliechick.hivo
 
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.media.MediaRecorder
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.provider.Telephony.Mms.Part.FILENAME
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.Manifest.permission.RECORD_AUDIO
 import android.support.v4.app.ActivityCompat
-import android.content.pm.PackageManager
 import android.support.v4.content.ContextCompat
-import android.view.View
-import org.jetbrains.anko.toast
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
-
-
-
+import java.io.IOException
+import android.Manifest.permission.RECORD_AUDIO
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 
 class MainActivity : AppCompatActivity() {
-
-    val filename = Environment.getExternalStorageDirectory().absolutePath + "/AudioRecording.3gp"
-    val REQUEST_AUDIO_PERMISSION_CODE = 402
-    var isRecording = false
-
+    private var startbtn: Button? = null
+    private var stopbtn: Button? = null
+    private var playbtn: Button? = null
+    private var stopplay: Button? = null
+    private var mRecorder: MediaRecorder? = null
+    private var mPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        startbtn = findViewById(R.id.btnRecord)
+        stopbtn = findViewById(R.id.btnStop)
+        playbtn = findViewById(R.id.btnPlay)
+        stopplay = findViewById(R.id.btnStopPlay)
+        stopbtn!!.isEnabled = false
+        playbtn!!.isEnabled = true
+        stopplay!!.isEnabled = true
+        mFileName = Environment.getExternalStorageDirectory().absolutePath
+        mFileName += "/AudioRecording.3gp"
 
-    private val recorder = MediaRecorder()
+        startbtn!!.setOnClickListener {
+            if (CheckPermissions()) {
+                stopbtn!!.isEnabled = true
+                startbtn!!.isEnabled = false
+//                playbtn!!.isEnabled = false
+//                stopplay!!.isEnabled = false
+                mRecorder = MediaRecorder()
+                mRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+                mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                mRecorder!!.setOutputFile(mFileName)
+                try {
+                    mRecorder!!.prepare()
+                } catch (e: IOException) {
+                    Log.e(LOG_TAG, "prepare() failed")
+                }
 
-    fun record(v: View) {
-        if (isRecording) {
-            toast("Stopping recording")
-            stopRecording()
-        } else {
-            toast("Starting recording")
-            if (checkPermissions()) startRecordingToFile(filename)
-            else requestPermissions()
+                mRecorder!!.start()
+                Toast.makeText(applicationContext, "Recording Started", Toast.LENGTH_LONG).show()
+            } else {
+                RequestPermissions()
+            }
         }
-    }
-
-    private fun startRecordingToFile(filename: String) {
-        recorder.apply {
-            setAudioSource(MediaRecorder.AudioSource.DEFAULT)
-            setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS) // "useful for streaming" according to docs
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-            setOutputFile(filename)
-            prepare()
-            start()
+        stopbtn!!.setOnClickListener {
+            stopbtn!!.isEnabled = false
+            startbtn!!.isEnabled = true
+//            playbtn!!.isEnabled = true
+//            stopplay!!.isEnabled = true
+            mRecorder!!.stop()
+            mRecorder!!.release()
+            mRecorder = null
+            Toast.makeText(applicationContext, "Recording Stopped", Toast.LENGTH_LONG).show()
         }
-        isRecording = true
-    }
-
-    private fun stopRecording() {
-        recorder.apply {
-            stop()
-//            reset()  // You can reuse the object by going back to setAudioSource() step
-            release() // Now the object cannot be reused
+        playbtn!!.setOnClickListener {
+            stopbtn!!.isEnabled = false
+            startbtn!!.isEnabled = true
+//            playbtn!!.isEnabled = false
+//            stopplay!!.isEnabled = true
+            mPlayer = MediaPlayer()
+            try {
+                mPlayer!!.setDataSource(mFileName)
+                mPlayer!!.prepare()
+                mPlayer!!.start()
+                Toast.makeText(applicationContext, "Recording Started Playing", Toast.LENGTH_LONG).show()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+        stopplay!!.setOnClickListener {
+            mPlayer!!.release()
+            mPlayer = null
+            stopbtn!!.isEnabled = false
+            startbtn!!.isEnabled = true
+//            playbtn!!.isEnabled = true
+//            stopplay!!.isEnabled = false
+            Toast.makeText(applicationContext, "Playing Audio Stopped", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
-            REQUEST_AUDIO_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+            REQUEST_AUDIO_PERMISSION_CODE -> if (grantResults.size > 0) {
                 val permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED
                 val permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED
                 if (permissionToRecord && permissionToStore) {
@@ -75,17 +108,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    private fun checkPermissions(): Boolean {
+    fun CheckPermissions(): Boolean {
         val result = ContextCompat.checkSelfPermission(applicationContext, WRITE_EXTERNAL_STORAGE)
         val result1 = ContextCompat.checkSelfPermission(applicationContext, RECORD_AUDIO)
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun requestPermissions() = ActivityCompat.requestPermissions(
-        this@MainActivity,
-        arrayOf(RECORD_AUDIO, WRITE_EXTERNAL_STORAGE),
-        REQUEST_AUDIO_PERMISSION_CODE
-    )
+    private fun RequestPermissions() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(RECORD_AUDIO, WRITE_EXTERNAL_STORAGE),
+            REQUEST_AUDIO_PERMISSION_CODE
+        )
+    }
 
+    companion object {
+        private val LOG_TAG = "AudioRecording"
+        private var mFileName: String? = null
+        val REQUEST_AUDIO_PERMISSION_CODE = 1
+    }
 }
