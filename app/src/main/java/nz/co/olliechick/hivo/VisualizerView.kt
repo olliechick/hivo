161ca6ada68c370d61b2bc5e5f43edd095a2c32b
Ollie Chick <och26@uclive.ac.nz>
2019-08-11 17:25:49 +1200

    Make array of vectors only just big enough to fit the number of lines displayable on the screen
    After reaching the end of the array, it overwrites the line that just
    disappeared off the screen

--- app/src/main/java/nz/co/olliechick/hivo/VisualizerView.kt
+++ app/src/main/java/nz/co/olliechick/hivo/VisualizerView.kt
@@ -2,7 +2,6 @@ package nz.co.olliechick.hivo
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -13,19 +12,18 @@ class VisualizerView(context: Context, attrs: AttributeSet) : View(context, attr
     private var vectors: FloatArray? = null
     private var insertIdx = 0
     private val linePaint: Paint = Paint()
-    private var screen_width: Int = 0
-    private var screen_height: Int = 0
+    private var screenWidth: Int = 0
+    private var screenHeight: Int = 0
 
     init {
         linePaint.color = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
         linePaint.strokeWidth = 1F
     }
 
-    override fun onSizeChanged(width: Int, h: Int, oldw: Int, oldh: Int) {
-        screen_width = width
-        screen_height = h
-        vectors = FloatArray(this.screen_width * 4 * 100) // xxyy for each line across the screen_width
-        //todo come up with a better solution that just giving the array 100x more space
+    override fun onSizeChanged(width: Int, height: Int, oldw: Int, oldh: Int) {
+        screenWidth = width
+        screenHeight = height
+        vectors = FloatArray(this.screenWidth * 4) // xxyy for each line across the screenWidth
     }
 
     /**
@@ -33,27 +31,48 @@ class VisualizerView(context: Context, attrs: AttributeSet) : View(context, attr
      */
     fun addAmplitude(amplitude: Int) {
         invalidate()
-        val scaledHeight = amplitude.toFloat() / MAX_AMPLITUDE * (screen_height - 1)
+        val scaledHeight = amplitude.toFloat() / MAX_AMPLITUDE * (screenHeight - 1)
         var vectorIdx = 0
 
-        while (vectorIdx < insertIdx * 4) {
-            // Decrement x value of each other line
-            val newX = vectors?.get(vectorIdx)!! - 1
-            if (newX < 0) {
-                vectors?.set(vectorIdx++, 0f) // x
-                vectors?.set(vectorIdx++, 0f) // y
-            } else {
-                vectors?.set(vectorIdx, newX) // x
-                vectorIdx += 2
+        if (insertIdx < screenWidth) {
+            // Initial phase, first lines on canvas
+
+            while (vectorIdx < insertIdx * 4) {
+                // Decrement x value of each other line
+                val newX = vectors?.get(vectorIdx)!! - 1
+                if (newX < 0) {
+                    vectors?.set(vectorIdx++, 0f) // x
+                    vectors?.set(vectorIdx++, 0f) // y
+                } else {
+                    vectors?.set(vectorIdx, newX) // x
+                    vectorIdx += 2
+                }
+            }
+
+            vectors?.set(vectorIdx++, screenWidth.toFloat() - 1) // x0
+            vectors?.set(vectorIdx++, (screenHeight.toFloat() - scaledHeight) / 2)   // y0
+            vectors?.set(vectorIdx++, screenWidth.toFloat() - 1)   // x1
+            vectors?.set(vectorIdx, (screenHeight.toFloat() + scaledHeight) / 2)  // y1
+
+            insertIdx++
+
+        } else {
+            // After canvas is filled up
+
+            while (vectorIdx < (insertIdx - 1) * 4) {
+                // Decrement x value of each other line
+                val newX = vectors?.get(vectorIdx)!! - 1
+                if (newX < 0) {
+                    vectors?.set(vectorIdx++, screenWidth.toFloat() - 1) // x0
+                    vectors?.set(vectorIdx++, (screenHeight.toFloat() - scaledHeight) / 2)   // y0
+                    vectors?.set(vectorIdx++, screenWidth.toFloat() - 1)   // x1
+                    vectors?.set(vectorIdx++, (screenHeight.toFloat() + scaledHeight) / 2)  // y1
+                } else {
+                    vectors?.set(vectorIdx, newX) // x
+                    vectorIdx += 2
+                }
             }
         }
-
-        vectors?.set(vectorIdx++, screen_width.toFloat() - 1) // x0
-        vectors?.set(vectorIdx++, (screen_height.toFloat() - scaledHeight) / 2)   // y0
-        vectors?.set(vectorIdx++, screen_width.toFloat() - 1)   // x1
-        vectors?.set(vectorIdx, (screen_height.toFloat() + scaledHeight) / 2)  // y1
-
-        insertIdx++
     }
 
     override fun onDraw(canvas: Canvas) {
