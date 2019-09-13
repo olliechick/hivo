8d4fdec7a4b134e36068b50291f6495b02992039
Ollie Chick <och26@uclive.ac.nz>
2019-09-13 14:15:54 +1200

    Get scheduled recordings to start at scheduled time

--- app/src/main/java/nz/co/olliechick/hivo/SchedRecordingReceiver.kt
+++ app/src/main/java/nz/co/olliechick/hivo/SchedRecordingReceiver.kt
@@ -3,14 +3,11 @@ package nz.co.olliechick.hivo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import nz.co.olliechick.hivo.util.Recordings.Companion.startRecording
 
 
 class SchedRecordingReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d("FOO", "Received message ${intent.action}!")
-        // todo create notification
-
-        // todo start recording
+        startRecording(context)
     }
 }
