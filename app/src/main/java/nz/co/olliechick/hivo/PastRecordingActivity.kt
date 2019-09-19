336549338463d7269c49a45b33607de5021a7e00
Ollie Chick <och26@uclive.ac.nz>
2019-09-19 13:50:54 +1200

    Add click listener for items in past recording list
    For now, it just toasts the name

--- app/src/main/java/nz/co/olliechick/hivo/PastRecordingActivity.kt
+++ app/src/main/java/nz/co/olliechick/hivo/PastRecordingActivity.kt
@@ -1,5 +1,6 @@
 package nz.co.olliechick.hivo
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,11 @@ import nz.co.olliechick.hivo.util.Database.Companion.initialiseDb
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import java.util.*
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+import android.view.GestureDetector
+import nz.co.olliechick.hivo.util.Ui.Companion.toast
+
 
 class PastRecordingActivity : AppCompatActivity() {
 
@@ -27,6 +33,59 @@ class PastRecordingActivity : AppCompatActivity() {
         supportActionBar?.title = getString(R.string.past_recordings)
 
         populateList()
+
+        list.addOnItemTouchListener(
+            RecyclerItemClickListener(this, list, object : RecyclerItemClickListener.OnItemClickListener {
+                override fun onItemClick(view: View, position: Int) {
+                    toast("Opening ${recordings[position].name}...")
+                }
+
+                override fun onLongItemClick(view: View?, position: Int) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
+        )
+    }
+
+
+    // adapted from https://stackoverflow.com/a/26196831/8355496
+    class RecyclerItemClickListener(
+        context: Context,
+        recyclerView: RecyclerView,
+        private val mListener: OnItemClickListener?
+    ) : RecyclerView.OnItemTouchListener {
+        interface OnItemClickListener {
+            fun onItemClick(view: View, position: Int)
+            fun onLongItemClick(view: View?, position: Int)
+        }
+
+        private var mGestureDetector: GestureDetector
+
+        init {
+            mGestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onSingleTapUp(e: MotionEvent) = true
+
+                override fun onLongPress(e: MotionEvent) {
+                    val child = recyclerView.findChildViewUnder(e.x, e.y)
+                    if (child != null && mListener != null) {
+                        mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child))
+                    }
+                }
+            })
+        }
+
+        override fun onInterceptTouchEvent(view: RecyclerView, e: MotionEvent): Boolean {
+            val childView = view.findChildViewUnder(e.x, e.y)
+            if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+                mListener.onItemClick(childView, view.getChildAdapterPosition(childView))
+                return true
+            }
+            return false
+        }
+
+        override fun onTouchEvent(view: RecyclerView, motionEvent: MotionEvent) {}
+
+        override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
     }
 
     private fun populateList() {
